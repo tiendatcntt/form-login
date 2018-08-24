6d5b4ae35617fbb66c5b8a14e4acea32c5a233d0
Pham Tien Dat <pham.tien.dat@kloon.vn>
2018-08-24 15:17:12 +0700

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { AbstractControl, FormGroup, FormArray, FormControl, Validators } from '../../node_modules/@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'form-login';
+export class AppComponent{
+  title  = 'form-login';
+  /*email: string;
+  emailControl: AbstractControl;
+  form: FormGroup;
+
+  ngOnInit(){
+    this.form=new FormGroup(
+      {email: new FormControl('', Validators.compose([Validators.required, Validators.email]))}
+    );
+    this.emailControl=this.form.controls['email']
+  }*/
 }
